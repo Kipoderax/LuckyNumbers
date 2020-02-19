c8e864cd026f2d5d7b3a03e521450eaca363014d
Kipoderax <detroid90@wp.pl>
2020-02-19 16:58:16 +0100

    user dtos

---
+++ LuckyNumbers.API/Dtos/UserDetailsDto.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace LuckyNumbers.API.Dtos
+{
+    public class UserDetailsDto
+    {
+        public string username { get; set; }
+        public DateTime lastLogin { get; set; }
+        public DateTime created { get; set; }
+        public int amountOfThree { get; set; }
+        public int amountOfFour { get; set; }
+        public int amountOfFive { get; set; }
+        public int amountOfSix { get; set; }
+        public int betsSended { get; set; }
+        public int level { get; set; }
+        public int experience { get; set; }
+    }
+}
---
+++ LuckyNumbers.API/Dtos/UserStatisticsDto.cs
@@ -0,0 +1,14 @@
+namespace LuckyNumbers.API.Dtos
+{
+    public class UserStatisticsDto
+    {
+        public string username { get; set; }
+        public int betsSended { get; set; }
+        public int amountOfThree { get; set; }
+        public int amountOfFour { get; set; }
+        public int amountOfFive { get; set; }
+        public int amountOfSix { get; set; }
+        public int level { get; set; }
+        public int experience { get; set; }
+    }
+}
