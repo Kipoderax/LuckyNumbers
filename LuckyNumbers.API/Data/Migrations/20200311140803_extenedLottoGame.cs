124044ea73eaff812358beacd5d6f5f7ed0d877b
Kipoderax <detroid90@wp.pl>
2020-03-11 15:13:27 +0100

    extended GameLotto database

---
+++ LuckyNumbers.API/Data/Migrations/20200311140803_extenedLottoGame.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LuckyNumbers.API.Migrations
+{
+    public partial class extenedLottoGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "profit",
+                table: "lottoGames",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "profit",
+                table: "lottoGames");
+        }
+    }
+}
