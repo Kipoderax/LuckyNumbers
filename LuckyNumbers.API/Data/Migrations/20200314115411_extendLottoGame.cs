bc509a1cac552c8bccecc0afd1de73099d2cf273
Kipoderax <detroid90@wp.pl>
2020-03-14 13:38:54 +0100

    extend lottoGame Entity

---
+++ LuckyNumbers.API/Data/Migrations/20200314115411_extendLottoGame.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LuckyNumbers.API.Migrations
+{
+    public partial class extendLottoGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "resultCheck",
+                table: "lottoGames",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "resultCheck",
+                table: "lottoGames");
+        }
+    }
+}
